e84b339c5bbabe59370bf5ec73b32bed7cf4dd28
Tim Leonard <tim@programmingbytim.com>
2016-07-31 00:32:07 +0100

    More Travis-CI

--- Build/Generate.Linux.sh
+++ Build/Generate.Linux.sh
@@ -1,3 +1,3 @@
-../Binaries/linux_x64/microbuild generate make Config/Workspace.ini --verbose
+../Binaries/linux_x64/microbuild generate make Config/Workspace.ini
 cat ./ProjectFiles/MicroBuild/MicroBuild.Makefile
 
