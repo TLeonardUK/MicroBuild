06be265500b8adae4b52d72648e886f2b2c59c09
Tim <tim@programmingbytim.com>
2016-08-20 14:54:13 +0100

    Windows warning fix.

--- Source/MicroBuild/App/Ides/XCode/XCode_SchemaFile.cpp
+++ Source/MicroBuild/App/Ides/XCode/XCode_SchemaFile.cpp
@@ -41,6 +41,9 @@ bool XCode_SchemaFile::Generate(
     IdeHelper::BuildProjectMatrix& buildMatrix
 )
 {
+    UNUSED_PARAMETER(buildMatrix);
+    UNUSED_PARAMETER(projectFiles);
+
 	Platform::Path projectDirectory =
 		projectFile.Get_Project_Location();
 
