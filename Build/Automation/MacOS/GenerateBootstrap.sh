168c70b18d4c5ce233cc07b34423304c85134892
TLeonardUK <tim@programmingbytim.com>
2016-11-22 19:02:43 +0000

    Appveyor script updates.

---
+++ Build/Automation/MacOS/GenerateBootstrap.sh
@@ -0,0 +1,3 @@
+cp ../../../Binaries/MacOS/Shipping_x64/microbuild ../../../Binaries/MacOS/Shipping_x64/microbuild.bootstrap 
+../../../Binaries/MacOS/Shipping_x64/microbuild.bootstrap generate xcode Config/Workspace.ini
+
---
+++ Build/Automation/Windows/GitAddArtifacts_x64.bat
@@ -0,0 +1,32 @@
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild.exe
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-Core.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-Core.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-FreeImage.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-FreeImage.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibJpeg.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibJpeg.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibJxr.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibJxr.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibOpenJpeg.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibOpenJpeg.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibPng.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibPng.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibRawLite.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibRawLite.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibTiff4.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibTiff4.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibWebP.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-LibWebP.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-OpenExr.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-OpenExr.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-Schemas.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-Schemas.pdb
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-Zlib.lib
+git add -f ../../../Binaries/Windows/Shipping_x64/MicroBuild-Zlib.pdb
+
+
+
+
+
+
