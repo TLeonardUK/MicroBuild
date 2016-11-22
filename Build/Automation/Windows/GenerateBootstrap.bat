f2b1a46ef1fb0d7dfb131fb69943a8dce9788ea6
TLeonardUK <tim@programmingbytim.com>
2016-11-22 19:04:52 +0000

    Appveyor script updates.
    Former-commit-id: 359e6dfd95df865081126a2d7a88be564de6dae3

--- Build/Automation/Windows/GenerateBootstrap.bat
+++ Build/Automation/Windows/GenerateBootstrap.bat
@@ -1,3 +1,3 @@
 cp ..\..\..\Binaries\Windows\Shipping_x64\microbuild.exe ..\..\..\Binaries\Windows\Shipping_x64\microbuild.bootstrap.exe 
-..\..\..\Binaries\Windows\Shipping_x64\microbuild.bootstrap.exe generate vs2015 Config/Workspace.ini
+..\..\..\Binaries\Windows\Shipping_x64\microbuild.bootstrap.exe generate vs2015 ../../Config/Workspace.ini
 
