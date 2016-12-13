b8b09432ac489aad31e0ad1c1e7e24e0d3017697
Tim Leonard <tim@programmingbytim.com>
2016-12-13 18:36:03 +0000

    Update Build.sh

--- Build/Automation/Linux/Build.sh
+++ Build/Automation/Linux/Build.sh
@@ -1,2 +1,2 @@
-../../../Binaries/Linux/Shipping_x64/microbuild.bootstrap build -r -configuration=Shipping -p=x86 ../../Config/Workspace.ini
-../../../Binaries/Linux/Shipping_x64/microbuild.bootstrap build -r -configuration=Shipping -p=x64 ../../Config/Workspace.ini
+../../../Binaries/Linux/Shipping_x64/MicroBuild.bootstrap build -r -configuration=Shipping -p=x86 ../../Config/Workspace.ini
+../../../Binaries/Linux/Shipping_x64/MicroBuild.bootstrap build -r -configuration=Shipping -p=x64 ../../Config/Workspace.ini
