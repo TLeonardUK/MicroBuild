0bede447c4996986d7b8111cd9fe98a40bcc4cb3
TLeonardUK <tim@programmingbytim.com>
2017-02-08 21:52:34 +0000

    Added premake configuration for building when self-hosting is non-functional.

---
+++ Build/Bootstrap/Build.Windows.bat
@@ -0,0 +1,30 @@
+@echo off
+
+set OriginalDir=%CD%
+mkdir ProjectFIles
+cd ProjectFiles
+
+..\Premake\premake5.windows.exe vs2015 --file=..\Workspace.premake 
+if %ERRORLEVEL% NEQ 0 GOTO ErrorHandler
+
+CALL "%VS140COMNTOOLS%\vsvars32.bat"
+if %ERRORLEVEL% EQU 0 GOTO GotVars
+CALL "%VS120COMNTOOLS%\vsvars32.bat"
+if %ERRORLEVEL% EQU 0 GOTO GotVars
+CALL "%VS110COMNTOOLS%\vsvars32.bat"
+if %ERRORLEVEL% EQU 0 GOTO GotVars
+
+GOTO ErrorHandler
+
+:GotVars
+msbuild MicroBuild.sln /t:rebuild /p:Configuration=Release /p:Platform=x64
+if %ERRORLEVEL% NEQ 0 GOTO ErrorHandler
+msbuild MicroBuild.sln /t:rebuild /p:Configuration=Release /p:Platform=x86
+if %ERRORLEVEL% NEQ 0 GOTO ErrorHandler
+
+cd %OriginalDir%
+CMD /C EXIT 0
+
+:ErrorHandler
+cd %OriginalDir%
+CMD /C EXIT %ERRORLEVEL%
