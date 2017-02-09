51855d420dbcc3c522448f63436441186ff76477
Tim Leonard <tim@programmingbytim.com>
2017-02-09 21:29:19 +0000

    Update GenerateBootstrap.sh

--- Build/Automation/MacOS/GenerateBootstrap.sh
+++ Build/Automation/MacOS/GenerateBootstrap.sh
@@ -1,3 +1,4 @@
-cp ../../../Binaries/MacOS/Shipping_x64/MicroBuild ../../../Binaries/MacOS/Shipping_x64/MicroBuild.bootstrap 
+../../../Build/Bootstrap/Build.MacOS.sh
+cp ../../../Build/Bootstrap/ProjectFiles/MicroBuild/Shipping_x64/bin/MicroBuild ../../../Binaries/MacxOS/Shipping_x64/MicroBuild.bootstrap 
 ../../../Binaries/MacOS/Shipping_x64/MicroBuild.bootstrap generate xcode ../../Config/Workspace.ini
 
