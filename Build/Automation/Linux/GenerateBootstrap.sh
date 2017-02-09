22510d5614ea09ec2758ca6d83440af46b3cf458
Tim Leonard <tim@programmingbytim.com>
2017-02-09 22:36:01 +0000

    Update GenerateBootstrap.sh

--- Build/Automation/Linux/GenerateBootstrap.sh
+++ Build/Automation/Linux/GenerateBootstrap.sh
@@ -1,4 +1,6 @@
-../../../Build/Bootstrap/Build.Linux.sh
+cd ../../../Build/Bootstrap/
+Build.Linux.sh
+cd ../../Build/Automation/Linux
 cp ../../../Build/Bootstrap/ProjectFiles/MicroBuild/Shipping_x64/bin/MicroBuild ../../../Binaries/Linux/Shipping_x64/MicroBuild.bootstrap 
 ../../../Binaries/Linux/Shipping_x64/MicroBuild.bootstrap generate make ../../Config/Workspace.ini
 
