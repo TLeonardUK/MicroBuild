e586d1ea522a8afb60b83107762384c089211c8d
Tim Leonard <tim@programmingbytim.com>
2017-02-09 21:29:31 +0000

    Update GenerateBootstrap.sh
    Former-commit-id: c37878400c8105a2ca3834deb25da8facbfda971

--- Build/Automation/MacOS/GenerateBootstrap.sh
+++ Build/Automation/MacOS/GenerateBootstrap.sh
@@ -1,4 +1,4 @@
 ../../../Build/Bootstrap/Build.MacOS.sh
-cp ../../../Build/Bootstrap/ProjectFiles/MicroBuild/Shipping_x64/bin/MicroBuild ../../../Binaries/MacxOS/Shipping_x64/MicroBuild.bootstrap 
+cp ../../../Build/Bootstrap/ProjectFiles/MicroBuild/Shipping_x64/bin/MicroBuild ../../../Binaries/MacOS/Shipping_x64/MicroBuild.bootstrap 
 ../../../Binaries/MacOS/Shipping_x64/MicroBuild.bootstrap generate xcode ../../Config/Workspace.ini
 
