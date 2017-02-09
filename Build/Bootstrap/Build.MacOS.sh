5f9677db7ae0df08d02614c2d10fcfb6dae453e7
TLeonardUK <infinitus@zombiegrinder.com>
2017-02-09 13:20:03 -0800

    MacOS bootrap script.
    Former-commit-id: 2aecb1f42346cfb8608f889c2e1a150a9bc586db

---
+++ Build/Bootstrap/Build.MacOS.sh
@@ -0,0 +1,14 @@
+mkdir ProjectFiles
+cd ProjectFiles
+
+../Premake/premake5.macos gmake --file=../Workspace.premake
+make --file=Makefile
+
+cd ..
+~                                                                                                                                                                                                                                                                                                                                                                                                                                 
+~                                                                                                                                                                                                                                                                                                                                                                                                                                 
+~                                                                                                                                                                                                                                                                                                                                                                                                                                 
+~                                                                                                                                                                                                                                                                                                                                                                                                                                 
+~                                                                                                                                                                                                                                                                                                                                                                                                                                 
+~                                                                                                                                                                                                                                                                                                                                                                                                                                 
+~                             
