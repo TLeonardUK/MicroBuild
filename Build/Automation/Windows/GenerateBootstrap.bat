71e64e26dbb2056fb6624faba2783507e6b64496
Tim Leonard <tim@programmingbytim.com>
2017-03-27 14:12:18 +0100

    Update GenerateBootstrap.bat
    Former-commit-id: 0c23fd24329bb0fef342b61582e28ea9835f8e86

--- Build/Automation/Windows/GenerateBootstrap.bat
+++ Build/Automation/Windows/GenerateBootstrap.bat
@@ -1,4 +1,4 @@
-..\..\..\Build\Bootstrap\Build.Windows.sh
+..\..\..\Build\Bootstrap\Build.Windows.bat
 cp ..\..\..\Build\Bootstrap\ProjectFiles\MicroBuild\Shipping_x64\bin\microbuild.exe ..\..\..\Binaries\Windows\Shipping_x64\microbuild.bootstrap.exe 
 ..\..\..\Binaries\Windows\Shipping_x64\microbuild.bootstrap.exe generate vs2015 ../../Config/Workspace.ini
 
