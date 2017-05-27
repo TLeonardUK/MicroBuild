b4b9419ad42442f8aca6f1ba471610c1399b136c
TLeonardUK <tim@programmingbytim.com>
2017-05-27 17:37:23 +0100

    Bootstrap fixes

--- Build/Bootstrap/Build.Linux.sh
+++ Build/Bootstrap/Build.Linux.sh
@@ -4,4 +4,21 @@ cd ProjectFiles
 ../Premake/premake5.linux gmake --file=../Workspace.premake
 make config=shipping_x64 --file=Makefile
 
+OUT=$?
+if [ $OUT -neq 0 ];then
+	cd ..
+	echo "make returned exit code $OUT, aborting ..."
+	exit 1
+fi
+
+../Premake/premake5.linux gmake --file=../Workspace.premake
+make config=shipping_x86 --file=Makefile
+
+OUT=$?
+if [ $OUT -neq 0 ];then
+	cd ..
+	echo "make returned exit code $OUT, aborting ..."
+	exit 1
+fi
+
 cd ..
--- Build/Bootstrap/Build.Windows.bat
+++ Build/Bootstrap/Build.Windows.bat
@@ -4,9 +4,11 @@ set OriginalDir=%CD%
 mkdir ProjectFIles
 cd ProjectFiles
 
-..\Premake\premake5.windows.exe vs2015 --file=..\Workspace.premake 
+..\Premake\premake5.windows.exe vs2017 --file=..\Workspace.premake 
 if %ERRORLEVEL% NEQ 0 GOTO ErrorHandler
 
+CALL "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvars32.bat"
+if %ERRORLEVEL% EQU 0 GOTO GotVars
 CALL "%VS140COMNTOOLS%\vsvars32.bat"
 if %ERRORLEVEL% EQU 0 GOTO GotVars
 CALL "%VS120COMNTOOLS%\vsvars32.bat"
@@ -24,7 +26,11 @@ if %ERRORLEVEL% NEQ 0 GOTO ErrorHandler
 
 cd %OriginalDir%
 CMD /C EXIT 0
+goto Finished
 
 :ErrorHandler
+echo "msbuild returned exit code %ERRORLEVEL%, aborting ..."
 cd %OriginalDir%
 CMD /C EXIT %ERRORLEVEL%
+
+:Finished
