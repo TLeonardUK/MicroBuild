6d4efd3f079c80b62b3e619b4119e529be01ecaa
TLeonardUK <tim@programmingbytim.com>
2017-05-29 23:33:00 +0100

    TravisCI Work

--- Build/Build.Linux.sh
+++ Build/Build.Linux.sh
@@ -4,12 +4,12 @@
 # Builds a MicroBuild binary for windows.
 # ------------------------------------------------------------------------------
 
-set BuildDir="$(pwd)"
-set BinariesDir=$BuildDir/../Binaries
-set BootstrapDir=$BuildDir/Bootstrap
-set VersionNumber=$MB_BUILD_VERSION
+BuildDir="$(pwd)"
+BinariesDir=$BuildDir/../Binaries
+BootstrapDir=$BuildDir/Bootstrap
+VersionNumber=$MB_BUILD_VERSION
 
-if [ "$VersionNumber" == "" ]; then set VersionNumber="99.99"; fi
+if [ "$VersionNumber" == "" ]; then VersionNumber="99.99"; fi
 
 echo BuildDir $BuildDir
 echo BinariesDir: $BinariesDir
